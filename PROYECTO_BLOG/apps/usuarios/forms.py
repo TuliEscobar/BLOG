b88542acdc4ca70c38d865b53b7069fe7e5969c8
Tuli <tuliescobarr@gmail.com>
2021-12-17 14:23:34 -0300

    creacion de usuario

--- PROYECTO_BLOG/apps/usuarios/forms.py
+++ PROYECTO_BLOG/apps/usuarios/forms.py
@@ -3,7 +3,15 @@ from django.contrib.auth.forms import UserCreationForm
 from . models import Usuario
 
 
-class UsuarioForm(UserCreationForm):
+class UsuarioForm(UserCreationForm):   
+    username = forms.CharField(label="Usuario", widget=forms.TextInput(attrs={"class": "form-control"}))
+    first_name = forms.CharField(label="Nombre", widget=forms.TextInput(attrs={"class": "form-control"}))
+    last_name = forms.CharField(label="Apellido", widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.CharField(label="Email", widget=forms.TextInput(attrs={"class": "form-control"}))
+    dni = forms.CharField(label="Dni", widget=forms.TextInput(attrs={"class": "form-control"}))
+    
+    password1 = forms.CharField(label="Contraseña", widget=forms.TextInput(attrs={"class": "form-control"}))
+    password2 = forms.CharField(label="Confirme Contraseña", widget=forms.TextInput(attrs={"class": "form-control"}))
 
     
     class Meta:
